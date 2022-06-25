9ba222f066aa1baae541eff394f7826aa4e64490
uillian cerqueira <wbassplay@gmail.com>
2022-06-24 22:25:29 -0300

    strings avancados

---
+++ sript.js
@@ -0,0 +1,6 @@
+console.log("Primeira linha \n segunda linha");
+console.log("O meu nome e 'uillian'");
+console.log('O meu nome e "Roberta"');
+console.log('O meu nome e "uillian"');
+console.log(`A multiplicacao de 5 por 3 é ${5*3}`);
+console.log("O " + "meu" + "nome" + "é" + " Uillian ");
